54751aae072787801e013eb88b6ae0e820bef3d8
CodeMax <max.spelsberg@gmail.com>
2016-06-22 11:01:14 +0200

    third commit (see first/second)

--- article/article-rest/src/main/java/de/fhm/akfo/shop/article/rest/to/ArticleDtoMapper.java
+++ article/article-rest/src/main/java/de/fhm/akfo/shop/article/rest/to/ArticleDtoMapper.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package de.fhm.akfo.shop.article.rest.to;
 
 import de.fhm.akfo.shop.article.rest.impl.ArticleResource;
@@ -23,31 +22,3 @@ public class ArticleDtoMapper extends JaxRsResourceMapperSupport<ArticleTo, Arti
     }
 
 }
-=======
-package de.fhm.akfo.shop.article.rest.dto;
-
-import de.fhm.akfo.shop.article.rest.impl.ArticleResource;
-import de.fhm.akfo.shop.article.service.api.dto.ArticleDto;
-
-/**
- * Mapper fuer das Datentransportobjekt {@link ArticleDto} auf {@link Article}
- * Entitaet und umgekehrt.
- * 
- * @author Spelsberg.Maximilian
- */
-public class ArticleDtoMapper extends JaxRsResourceMapperSupport<ArticleDto, ArticleTO> {
-
-    public ArticleDtoMapper() {
-        super(ArticleResource.class, ArticleTO.class);
-    }
-
-    @Override
-    public ArticleTO toResource(ArticleDto entity) {
-    	ArticleTO resource = createResourceWithId(entity.getServicetemplateId(), entity);
-        resource.setName(entity.getName());
-        resource.setServicetemplateId(entity.getServicetemplateId());
-        return resource;
-    }
-
-}
->>>>>>> branch 'master' of https://github.com/CodeMax/Scalable-Shopsystem.git
--- article/article-rest/src/main/java/de/fhm/akfo/shop/article/rest/to/ArticleTo.java
+++ article/article-rest/src/main/java/de/fhm/akfo/shop/article/rest/to/ArticleTo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package de.fhm.akfo.shop.article.rest.to;
 
 import java.util.Date;
@@ -188,61 +187,3 @@ public class ArticleTo extends ResourceSupport {
 	}
 
 }
-=======
-package de.fhm.akfo.shop.article.rest.dto;
-
-import org.springframework.hateoas.ResourceSupport;
-
-/**
- * Article Business-Objekt.
- * 
- * @author Spelsberg.Maximilian
- */
-public class ArticleTO extends ResourceSupport {
-
-	/** Article-Id */
-	private Long articleId;
-	
-	/** Article-Name. */
-	private String name;
-	
-	
-	/**
-	 * Default-Konstruktor.
-	 */
-	public ArticleTO() {
-	}
-	
-
-	/**
-	 * Konstruktor zur Befuellung aller Attribute.
-	 * 
-	 * @param name Article-Name String
-	 */
-	public ArticleTO(String name, long id) {
-		this.name = name;
-		this.articleId = id;
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public long getServicetemplateId() {
-		return articleId;
-	}
-
-
-	public void setServicetemplateId(long id) {
-		this.articleId = id;
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/CodeMax/Scalable-Shopsystem.git
